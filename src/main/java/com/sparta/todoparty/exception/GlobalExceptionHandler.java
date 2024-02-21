f2d6d5d84b16844233af37f224ddbe8b7855ecbb
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-02-21 19:28:15 +0900

    feat(exception): Global 예외 처리

---
+++ src/main/java/com/sparta/todoparty/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.sparta.todoparty.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.concurrent.RejectedExecutionException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<RestApiException> handleIllegalArgumentException(IllegalArgumentException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler({RejectedExecutionException.class})
+    public ResponseEntity<RestApiException> handleRejectedExecutionException(RejectedExecutionException ex){
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(
+                restApiException,
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
