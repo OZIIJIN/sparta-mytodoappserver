66adcd451a6fa02eae9d9de193430da35a9f9d58
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-03-07 00:53:42 +0900

    JwtUtil 토큰 생성 테스트

--- src/test/java/com/sparta/todoparty/test/JwtUtilTest.java
+++ src/test/java/com/sparta/todoparty/test/JwtUtilTest.java
@@ -1,8 +1,12 @@
 package com.sparta.todoparty.test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import com.sparta.todoparty.jwt.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,5 +27,14 @@ public class JwtUtilTest implements CommonTest{
 		jwtUtil.init();
 	}//@BeforeEach를 사용해서 setUp
 
+	@DisplayName("토큰 생성")
+	@Test
+	void createToken() {
+		// when
+		String token = jwtUtil.createToken(TEST_USER_NAME);
+
+		// then
+		assertNotNull(token);
+	}
 
 }
