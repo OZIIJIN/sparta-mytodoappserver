e90b5d36302a46ebc66928c182301ca0238849b8
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-03-16 03:18:38 +0900

    CollaborationEntity setting

--- src/main/java/com/sparta/todoparty/collaboration/entity/CollaborationEntity.java
+++ src/main/java/com/sparta/todoparty/collaboration/entity/CollaborationEntity.java
@@ -1,5 +1,51 @@
 package com.sparta.todoparty.collaboration.entity;
 
-public class CollaborationEntity {
+import com.sparta.todoparty.common.TimeStamp;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "tb_collaboration")
+public class CollaborationEntity extends TimeStamp {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private Long fromUserId;
+
+	@Column(nullable = false)
+	private Long toUserId;
+
+	@Column(nullable = false)
+	private String todo;
+
+	@Column
+	private Boolean isCompleted;
+
+	@Builder
+	public CollaborationEntity(Long fromUserId, Long toUserId, String todo) {
+		this.fromUserId = fromUserId;
+		this.toUserId = toUserId;
+		this.todo = todo;
+		this.isCompleted = false;
+	}
+
+	public void updateTodo(String newTodo) {
+		this.todo = newTodo;
+	}
+
+	public void completed() {
+		this.isCompleted = true;
+	}
 }
